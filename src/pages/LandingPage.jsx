eb0246111d50f79ebb71ef029dcfd72515a7b8f8
AkshaySoman55 <akshaysoman56@gmail.com>
2024-03-18 23:32:53 +0530

    changes added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -25,9 +25,9 @@ function LandingPage() {
 
       <div className='container d-flex justify-content-center align-items-center mt-5 flex-column mb-5'>
         <h3>Features</h3>
-        <div className='row d-flex justify-content-evenly me-5'>
+        <div className='row d-flex justify-content-center align-items-center'>
 
-          <div className="col-md-4 px-4">
+          <div className="col-md-4">
           <Card className='p-4 m-3' style={{ width: '16rem' }}>
           <Card.Img variant="top" style={{width:'100%', height:'300px'}} src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDJndHV5bW1qeHVydmQ0dGZnNXEycXJjOXo3YTBzYW45aTcweXcxciZlcD12MV9naWZzX3NlYXJjaCZjdD1n/4oMoIbIQrvCjm/200.webp" />
       <Card.Body>
@@ -40,7 +40,7 @@ function LandingPage() {
           </Card>
           </div>
 
-          <div className="col-md-4 px-4">
+          <div className="col-md-4">
           <Card className='p-4 m-3' style={{ width: '16rem'  }}>
       <Card.Img variant="top" style={{width:'100%', height:'300px'}} src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZm5qNXZqM3Q3bXc5d294bWFjczFzbjJvZzFybG5obTZxcm5kcngyNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MF4gHGVeCOXAc/giphy.gif" />
       <Card.Body>
@@ -53,7 +53,7 @@ function LandingPage() {
           </Card>
           </div>
 
-          <div className="col-md-4 px-4">
+          <div className="col-md-4">
           <Card className='p-4 m-3'  style={{ width: '16rem' }}>
           <Card.Img variant="top" style={{width:'100%', height:'300px'}} src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExa3d0NjJuY3o1MzdrdHNxMndzMjZtdW84NDlwa29ybTJlbnBvdXZ1NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/tqfS3mgQU28ko/giphy.gif" />
       <Card.Body>
